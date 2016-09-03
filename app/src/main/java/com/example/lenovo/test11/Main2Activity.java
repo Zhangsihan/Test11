3905f940a9418202b609ce158ba0fc339c13caaf
Zhangsihan <13691499730@163.com>
2016-09-03 14:41:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/test11/Main2Activity.java
@@ -0,0 +1,33 @@
+package com.example.lenovo.test11;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+        Button button=(Button)findViewById(R.id.buttonReturn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=getIntent();
+                String name=intent.getStringExtra("name");
+                Integer age=intent.getIntExtra("age", 20);
+                intent.putExtra("result","姓名:"+name+" 年龄"+age);
+                setResult(0,intent);
+                finish();
+            }
+        });
+
+    }
+
+}
